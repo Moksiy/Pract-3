9f4f89c7df0436be1b005004d6812c0e6d74ac7a
Moksiy <MaksAligin@yandex.ru>
2019-05-26 12:33:53 +0300

    Готово

--- Pract3/Pract3/MainWindow.xaml.cs
+++ Pract3/Pract3/MainWindow.xaml.cs
@@ -29,21 +29,119 @@ namespace Pract3
 
     public class Information
     {
+        public int[] Coordinates { get; set; }
         public int Tops { get; set; }
         public double S { get; set; }
         public double P { get; set; }
     }
 
-    public class Data
+    public class Data : Window
     {
         public string FileName { get; set; }
-        public List<int> Coord = new List<int>();
-        public List<List<int>> list = new List<List<int>>();
-        public void AddCoordinates(int x)
+        public int[] Coord;
+        public List<Information> list = new List<Information>();
+        public void AddCoordinates(int[] coord)
         {
-            list.Add(new List<int>(x));
+            list.Add(new Information());
+            list[list.Count - 1].Coordinates = coord;
+            list[list.Count - 1].Tops = (coord.Length) / 2;
+            list[list.Count - 1].P = CalculateP(list[list.Count - 1].Tops, coord);
+            list[list.Count - 1].S = CalculateS(list[list.Count - 1].Tops, coord);
         }
-        List<Information> result = new List<Information>();
+
+        //Метод расчета периметра
+        private double CalculateP(int x, int[] coord)
+        {
+            double result = 0;
+            if (x == 1)
+            {
+                //Добавить что-то
+            }
+            else
+            {
+                for (int i = 0; i < (x-1)*2; i+=2)
+                {
+                    result += Linelength(coord[i], coord[i+1], coord[i+2], coord[i+3]);
+                }
+                result += Linelength(coord[0] ,coord[1] ,coord[coord.Length-2], coord[coord.Length-1]);
+            }
+            return result;
+        }
+
+        //Метод расчета площади
+        private double CalculateS(int x, int[] coord)
+        {
+            double result = 0;
+            if (x == 1 || x == 2)
+            {
+                //Добавить что-то
+            }
+            else
+            {
+                result = Square(coord);
+            }
+            return result;
+        }
+
+        //Расчет длины стороны
+        private double Linelength(int x1, int y1, int x2, int y2)
+        {
+            double result = Math.Sqrt(Math.Pow((x1-x2),2) + Math.Pow((y1-y2),2));
+            return result;
+        }
+
+        //Расчет площади 
+        private double Square(int[] coord)
+        {
+            double result = 0;
+            if (coord.Length / 2 > 3)
+            {
+                result = (SumCoord(coord) + (coord[coord.Length - 2] * coord[1]) - DiffCoord(coord) - (coord[0] * coord[coord.Length - 1])) / 2;
+            } else { result = ((coord[0] - coord[4]) *(coord[3] - coord[5]) - (coord[2] - coord[4]) *(coord[1] - coord[5])) / 2; }
+            return Math.Abs(result);
+        }
+
+        private double SumCoord(int[] coord)
+        {
+            double res = 0;
+            int count = coord.Length;
+            for (int i = 0; i < count-2; i+=2)
+            {
+                res += (coord[i]* coord[i+3]);
+            }
+            return res;
+        }
+
+        private double DiffCoord(int[] coord)
+        {
+            double res = 0;
+            int count = coord.Length;
+            for (int i = 1; i < count-2; i +=2)
+            {
+                res += (coord[i + 1] * coord[i]);
+            }
+            return res;
+        }
+
+        public int OutputTops(int index)
+        {
+            return list[index].Tops;
+        }
+
+        public double OutputS(int index)
+        {
+            return list[index].S;
+        }
+
+        public double OutputP(int index)
+        {
+            return list[index].P;
+        }
+
+        public int Count()
+        {
+            return list.Count;
+        }
     }
 
     public partial class MainWindow : Window
@@ -74,23 +172,35 @@ namespace Pract3
             if (data.FileName != null)
             {
                 Reader();
-            }else { MessageBox.Show("Не выбран файл"); }
+                OutputList();
+            }
+            else { MessageBox.Show("Не выбран файл"); }
         }
 
         //Считывание
         private void Reader()
         {
-            string x;
-            int i = 1;
             StreamReader reader = File.OpenText(data.FileName);
             string input;
             while ((input = reader.ReadLine()) != null)
             {
-                foreach (var st in input.Split())
+                int[] coordinates = input.Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x)).ToArray();
+                data.AddCoordinates(coordinates);
+            }
+        }
+
+        public void OutputList ()
+        {
+            for (int i = 0; i < data.Count(); i++)
+            {
+                Information element = new Information()
                 {
-                    x = st;
-                    data.AddCoordinates(Convert.ToInt32(x));
-                }
+                    Tops = data.OutputTops(i),
+                    S = data.OutputS(i),
+                    P = data.OutputP(i)
+                };
+
+                ListV.Items.Add(element);
             }
         }
     }
